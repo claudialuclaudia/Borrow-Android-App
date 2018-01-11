d24d07d67bb665b6f07fec4577aebc37bd90ad10
Claudia Lu <claudialuclaudia@gmail.com>
2018-01-11 09:49:45 -0500

    light toolbar & signin page begin

---
+++ app/src/main/java/cash/borrow/android/SignInActivity.java
@@ -0,0 +1,17 @@
+package cash.borrow.android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Lenovo on 1/11/2018.
+ */
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
