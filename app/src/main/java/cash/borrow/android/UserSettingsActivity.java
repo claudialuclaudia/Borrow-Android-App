46012ad8921378d3e23f5420a8b2efabdbdb25e5
Claudia Lu <claudialuclaudia@gmail.com>
2018-02-13 08:26:43 -0500

    working on new settings page

--- app/src/main/java/cash/borrow/android/UserSettingsActivity.java
+++ app/src/main/java/cash/borrow/android/UserSettingsActivity.java
@@ -1,17 +1,32 @@
 package cash.borrow.android;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
 
 import cash.borrow.android.model.UserInfoItem;
 
@@ -20,8 +35,13 @@ public class UserSettingsActivity extends AppCompatActivity implements View.OnCl
     private FirebaseAuth firebaseAuth;
     private DatabaseReference databaseReference;
 
+    private ImageView imageView;
     private EditText editTextName, editTextLocation;
-    private Button buttonSave, buttonUpload;
+    private Button buttonSave;
+
+    private Uri filePath;
+    private StorageReference storageReference;
+    private static final int PICK_IMAGE_REQUEST = 234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +49,21 @@ public class UserSettingsActivity extends AppCompatActivity implements View.OnCl
         setContentView(R.layout.activity_user_settings);
 
         firebaseAuth = FirebaseAuth.getInstance();
-
         databaseReference = FirebaseDatabase.getInstance().getReference();
+        storageReference = FirebaseStorage.getInstance().getReference();
 
         editTextName = (EditText) findViewById(R.id.editTextName);
         editTextLocation = (EditText) findViewById(R.id.editTextLocation);
         buttonSave = (Button) findViewById(R.id.buttonSave);
-        buttonUpload = (Button) findViewById(R.id.buttonUpload);
-
         buttonSave.setOnClickListener(this);
-        buttonUpload.setOnClickListener(this);
+
+        imageView = (ImageView) findViewById(R.id.imageViewUpload);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFileChooser();
+            }
+        });
     }
 
     private void saveInfo(){
@@ -50,20 +75,77 @@ public class UserSettingsActivity extends AppCompatActivity implements View.OnCl
 
         databaseReference.child(user.getUid()).setValue(userInfoItem);
 
+        uploadFile();
+
         Toast.makeText(this, "Info Saved", Toast.LENGTH_LONG).show();
 
+    }
+
+    private void uploadFile() {
+        if (filePath != null) {
+
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            StorageReference profileRef = storageReference.child("images/profile.jpg");
+
+            profileRef.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "File Uploaded", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                            progressDialog.setMessage(((int) progress) + "% Uploaded...");
+                        }
+                    })
+            ;
+        } else {
+            //display an error toast
+        }
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select an image"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null
+                && data.getData() != null) { //user have choosen an image
+            filePath = data.getData();
+            try {
+                Bitmap bitMap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitMap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     @Override
     public void onClick(View view) {
         if (view == buttonSave){
             saveInfo();
-        }
-
-        if (view == buttonUpload){
-            Intent myIntent = new Intent(UserSettingsActivity.this,
-                    UploadActivity.class);
-            startActivity(myIntent);
         }
     }
 }
