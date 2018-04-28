fa3e8c536110cf231f25f0855d6c681dc1139cca
Claudia Lu <claudialuclaudia@gmail.com>
2018-04-27 20:23:08 -0400

    more stripe info

--- app/src/main/java/cash/borrow/android/StripeActivity2.java
+++ app/src/main/java/cash/borrow/android/StripeActivity2.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.stripe.android.TokenCallback;
@@ -14,30 +15,32 @@ import com.stripe.android.Stripe;
 
 public class StripeActivity2 extends AppCompatActivity {
 
-    Button submitButton;
-    CardInputWidget mCardInputWidget;
+    private CardInputWidget mCardInputWidget;
+    private EditText customerName;
+    private EditText zipcode;
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stripe2);
 
+        mCardInputWidget = findViewById(R.id.card_input_widget);
         submitButton = findViewById(R.id.submit);
-        mCardInputWidget = (CardInputWidget) findViewById(R.id.card_input_widget);
-
-//        if (cardToSave == null) {
-//            mErrorDialogHandler.showError("Invalid Card Data");
-//            Toast.makeText(this, "Invalid Card Data", Toast.LENGTH_SHORT).show();
-//            return;
-//        }
-
-//        cardToSave.setName("Customer Name");
-//        cardToSave.setAddressZip("12345");
+        customerName = findViewById(R.id.customer_name);
+        zipcode = findViewById(R.id.zip_code);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Card cardToSave = mCardInputWidget.getCard();
+                cardToSave.setName(customerName.getText().toString().trim());
+                cardToSave.setAddressZip(zipcode.getText().toString().trim());
+                if (cardToSave == null) {
+//                    mErrorDialogHandler.showError("Invalid Card Data");
+                    Toast.makeText(StripeActivity2.this, "Invalid Card Data", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 //                Toast.makeText(StripeActivity2.this, "click!", Toast.LENGTH_SHORT).show();
                 Stripe stripe = new Stripe(StripeActivity2.this, "pk_test_6pRNASCoBOKtIshFeQd4XMUh");
                 stripe.createToken(
@@ -45,7 +48,7 @@ public class StripeActivity2 extends AppCompatActivity {
                         new TokenCallback() {
                             public void onSuccess(Token token) {
                                 // Send token to your server
-                                Toast.makeText(StripeActivity2.this, "Success!", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(StripeActivity2.this, token.getCard().toString(), Toast.LENGTH_SHORT).show();
                             }
                             public void onError(Exception error) {
                                 // Show localized error message
